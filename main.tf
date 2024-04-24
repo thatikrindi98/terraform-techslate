1814b69e80a370230bd3948870f5716d26b1eeeb
thatikrindi98 <thatikrindirakesh98@gmail.com>
2024-04-24 08:26:27 +0100

    first script created through varaiables and main file

--- main.tf
+++ main.tf
@@ -1,18 +1,17 @@
 provider "azurerm" {
   features {}
 }
-
-# Create a resource group
-resource "azurerm_resource_group" "rg" {
-  name     = "my-resource-group"
-  location = "eastus"
+# Create resource group
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
 }
 
-# Create a storage account
-resource "azurerm_storage_account" "st" {
-  name                     = "storageaccount2567"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# Create storage account
+resource "azurerm_storage_account" "main" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 }
